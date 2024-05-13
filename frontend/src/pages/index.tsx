c8525a4db1dbe08b19a9d22467809d86d1536dd9
JonasJallet <jonas.jallet@gmail.com>
2024-05-13 22:19:22 +0200

    typescript index

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,23 +1,20 @@
 import React, { useState } from "react";
 import CreateCountryForm from "@/components/CreateCountryForm";
 import { CountryList } from "@/components/CountryList";
+import { Country } from "@/graphql/generated/schema";
 
 export default function Home() {
-  const [newCountry, setNewCountry] = useState(null);
-
-  // Callback function to receive the new country object
-  const handleCountryAdded = (country) => {
+  const [newCountry, setNewCountry] = useState<Country | null>(null);
+  const handleCountryAdded = (country: Country) => {
     setNewCountry(country);
   };
 
   return (
     <>
-      {/* Pass the callback function to CreateCountryForm */}
       <CreateCountryForm onCountryAdded={handleCountryAdded} />
       <div className="flex flex-col gap-4 justify-center items-center">
         <h3>Here you can find a list of country that might interest you</h3>
       </div>
-      {/* Pass the new country object to CountryList */}
       <CountryList newCountry={newCountry} />
     </>
   );
