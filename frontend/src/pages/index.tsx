3693957a1a7289afc8e67c7031906e544186a3ca
JonasJallet <jonas.jallet@gmail.com>
2024-05-13 23:28:07 +0200

    don't forget screenshot

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -12,9 +12,6 @@ export default function Home() {
   return (
     <>
       <CreateCountryForm onCountryAdded={handleCountryAdded} />
-      <div className="flex flex-col gap-4 justify-center items-center">
-        <h3>Here you can find a list of country that might interest you</h3>
-      </div>
       <CountryList newCountry={newCountry} />
     </>
   );
